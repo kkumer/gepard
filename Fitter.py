FIX: derivatives of trans are now calculated in __init__ ADD: test of FitterBrain and Model.ComptonNeuralNetwork

Fitter.py
@@ -66,6 +66,18 @@ class FitterBrain(Fitter):
         self.inputs = theory.model.architecture[0]
         self.outputs = theory.model.architecture[-1]
         self.verbose = 0
+        # Numerical derivative of transformation function w.r.t. net output:
+        left = self.outputs * [1.0]
+        right = self.outputs * [1.0]
+        for pt in self.fitpoints:
+            deriv = []
+            for k in range(self.outputs):
+                left[k] = 1.2
+                deriv.append((theory.predict(pt, parameters={'outputvalue':tuple(left)}) -
+                         theory.predict(pt, parameters={'outputvalue':tuple(right)})) / 0.2)
+                # return left to default value of [1, 1, ...]
+                left[k] = 1.0
+            pt.deriv = np.array(deriv)
 
     def artificialData(self, datapoints, trainpercentage=70):
         """Create artificial data replica.
@@ -95,27 +107,7 @@ class FitterBrain(Fitter):
             # regardless of computer rounding behaviour
             ys[0] = pt.val + round(np.random.normal(0, pt.err, 1)[0], 5)
             # ys[1:] are zero and are never used.
-            # Numerical derivative of transformation function w.r.t. net output:
-            # FIXME: bit of a hack and SHOULD NOT BE REDONE for every makenet!!
-            left = self.outputs * [1.0]
-            right = tuple(self.outputs * [1.0])
-            deriv = []
-            for k in range(self.outputs):
-                left[k] = 1.2
-                deriv.append((self.theory.predict(pt, parameters={'outputvalue':tuple(left)}) -
-                         self.theory.predict(pt, parameters={'outputvalue':right})) / 0.2)
-                left[k] = 1.0
-            deriv = np.array(deriv)
-            #if self.outputs == 1:
-            #    deriv = np.array([(self.theory.predict(pt, parameters={'outputvalue':(1.2,)}) -
-            #             self.theory.predict(pt, parameters={'outputvalue':(1.0,)})) / 0.2])
-            #else:
-            #    deriv1 = (self.theory.predict(pt, parameters={'outputvalue':(1.2, 1.0)}) -
-            #             self.theory.predict(pt, parameters={'outputvalue':(1.0, 1.0)})) / 0.2
-            #    deriv2 = (self.theory.predict(pt, parameters={'outputvalue':(1.0, 1.2)}) -
-            #             self.theory.predict(pt, parameters={'outputvalue':(1.0, 1.0)})) / 0.2
-            #    deriv = np.array([deriv1, deriv2])
-            trans.map2pt[ys[0]] = (self.theory, pt, deriv)
+            trans.map2pt[ys[0]] = (self.theory, pt)
             if i < trainsize:
                 training.addSample(xs, ys)
             else:

pype.py
@@ -2,6 +2,9 @@
 
 import shelve
 
+import numpy as np
+#np.random.seed(68)
+
 import Model, Approach, Fitter
 import utils 
 import plots
@@ -29,12 +32,13 @@ fitpoints = data[31][12:14] + data[8][1:3] + data[30][2:4]  # test set
 HA17 = utils.select(data[34], criteria=['t == -0.17'])
 HA33 = utils.select(data[34], criteria=['t == -0.33'])
 #fitpoints = GLO1points + 6*data[30]
-fitpoints = GLO1points
+fitpoints = GLOpoints
 #fitpoints = data[5]
 
 ## [3] Create a theory
 
-m = Model.ModelNN(output_layer=['ImH', 'ReH', 'ReHt'])
+m = Model.ModelNNH()
+#m = Model.ModelNN(output_layer=['ImH', 'ReH', 'ImHt', 'ReHt'])
 t = Approach.hotfixedBMK(m)
 
 #m = Model.ModelDR()

tests.py
@@ -7,6 +7,7 @@ Just run 'nosetest' in the pype directory.
 import copy
 from nose.tools import *
 import numpy as np
+np.random.seed(68)
 
 import utils, Model, Approach, Fitter
 from results import DMGLO1  #use some testpars here?
@@ -30,12 +31,14 @@ testpoints = [data[31][12]] + [data[8][1]] + [data[29][2]] + [data[30][3]]
 fitpoints = data[31][12:14] + data[8][1:3] + data[30][2:4]
 
 def test_CFF():
+    """Calculate CFF H."""
     assert_almost_equal(m.ImH(pt0), 17.67971592396648)
     assert_almost_equal(m.ReH(pt0), -2.4699741916859592)
 
 test_CFF.one = 1
 
 def test_Xunp():
+    """Calculate basic cross section Xunp."""
     assert_almost_equal(t.Xunp(pt0, vars={'phi':1.}), 1.8934179005138172)
     ar = t.Xunp(pt0, vars={'phi':np.array([0.3, 1.1])})
     assert isinstance(ar, np.ndarray)
@@ -43,12 +46,12 @@ def test_Xunp():
     assert_almost_equal(ar[1], 1.9830803062066602)
 
 def test_Xunp2():
-    """New feature: any kinematic variable could be in vars."""
+    """Any kinematic variable can be in vars."""
     assert_almost_equal(t.Xunp(pt0, 
         vars={'phi':1., 'xB':0.07}), 3.0240991086297577)
 
 def test_Xunp3():
-    """New feature: ndarray of Q2 could be in vars."""
+    """ndarray of Q2 could be in vars."""
     ar = t.Xunp(pt0, vars={'phi':1, 'Q2':np.array([2.3, 2.5])})
     assert isinstance(ar, np.ndarray)
     assert_almost_equal(ar[0], 1.9818420283023106)
@@ -78,3 +81,17 @@ def test_fit2():
     assert_almost_equal(f.minuit.fval, 6.7638634368267949, 4)
 
 test_fit2.long = 1
+
+def test_fit_neural():
+    """Testing neural network fitting by FitterBrain."""
+    mNN = Model.ModelNN()
+    tNN = Approach.hotfixedBMK(mNN)
+    fNN = Fitter.FitterBrain(fitpoints, tNN)
+    fNN.fit(nnets=1)
+    chisq = 0.
+    for pt in fitpoints:
+        chisq = chisq + (
+                (getattr(t, pt.yaxis)(pt) - pt.val)**2 / pt.err**2 )
+    assert_almost_equal(chisq, 7.5464238699131956)
+
+#test_fit_neural.long = 2