137f4a08296dc844d9c1fe55810872ab730f5dea
Kresimir Kumericki <kkumer@phy.hr>
2010-04-27 22:05:50 +0000

    CHG: tests moved to directory 'test'

--- test/Approach_test.py
+++ test/Approach_test.py
@@ -1,21 +1,15 @@
-"""
-This is collection of tests. 
-Just run 'nosetest' in the pype directory.
-
-"""
 
 import copy
 from nose.tools import *
 import numpy as np
 
-import utils, Model, Approach, Fitter
+import utils, Model, Approach
 from results import DMGLO1  #use some testpars here?
 
 data = utils.loaddata('data/ep2epgamma')  
 
 m = Model.ModelDR()
 m.parameters.update(DMGLO1)
-m.release_parameters('bS', 'Mv')
 t = Approach.hotfixedBMK(m, optimization = False)
 
 # testing data point
@@ -24,10 +18,7 @@ pt0.in1polarization = 1
 pt0.in1charge = -1
 pt0.to_conventions(Approach.BMK)
 pt0.prepare(Approach.BMK)
-# testing data set
-testpoints = [data[31][12]] + [data[8][1]] + [data[29][2]] + [data[30][3]]
-# testing data set for fits
-fitpoints = data[31][12:14] + data[8][1:3] + data[30][2:4]
+
 
 def test_CFF():
     """Calculate CFF H."""
@@ -64,32 +55,3 @@ def test_Xunp4():
 
 test_Xunp4.newfeature = 1
 
-def test_fit():
-    """Testing set of fitting observables."""
-    chisq = 0.
-    for pt in testpoints:
-        chisq = chisq + (
-                (getattr(t, pt.yaxis)(pt) - pt.val)**2 / pt.err**2 )
-    assert_almost_equal(chisq, 5.115294374919535)
-
-
-def test_fit2():
-    """Testing actual fitting by FitterMinuit."""
-    f = Fitter.FitterMinuit(fitpoints, t)
-    f.fit()
-    chisq = t.chisq(fitpoints)[0]
-    assert_almost_equal(chisq, 6.7638690027046771)
-
-test_fit2.long = 1
-
-def test_fit_neural():
-    """Testing neural network fitting by FitterBrain."""
-    np.random.seed(68)
-    mNN = Model.ModelNN()
-    tNN = Approach.hotfixedBMK(mNN)
-    fNN = Fitter.FitterBrain(2*fitpoints, tNN, nnets=1, nbatch=6)
-    fNN.fit()
-    chisq = tNN.chisq(2*fitpoints)[0]
-    assert_almost_equal(chisq, 20.894673139809264)
-
-test_fit_neural.long = 1
