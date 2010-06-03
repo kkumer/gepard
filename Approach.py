FIX: Model.Gepard now works and passes basic      (non-fitting) tests of cross-section calculation

Approach.py
@@ -2,7 +2,7 @@ from IPython.Debugger import Tracer; debug_here = Tracer()
 
 import copy
 
-from numpy import sin, cos, pi, sqrt
+from numpy import sin, cos, pi, sqrt, array
 from scipy.special import gammainc
 
 import utils, quadrature
@@ -513,8 +513,15 @@ class BMK(Approach):
         to facilitate integration over it.
         
         """
-        pt.t = t
-        return PartialCrossSection(pt)
+        aux = []
+        for t_single in t:
+            pt.t = t_single
+            res = self.PartialCrossSection(pt)
+            #if debug == 2: print "t = %s  =>  dsig/dt = %s" % (t_single, res)
+            aux.append(res)
+
+        return array(aux)
+
 
     def PartialCrossSection(self, pt):
         """Partial DVCS cross section w.r.t. Mandelstam t."""
@@ -529,7 +536,7 @@ class BMK(Approach):
     def TotalCrossSection(self, pt):
         """Total DVCS cross section."""
 
-        res = quadrature.Hquadrature(lambda t: self.PartialCrossSection4int(t, pt), 0, 1)
+        res = quadrature.tquadrature(lambda t: self.PartialCrossSection4int(t, pt), -1, 0)
         return res
 
     def BCA0minusr1(self, pt):

test/Gepard_test.py
@@ -11,115 +11,61 @@ import utils, Model, Approach, Data
 from constants import Mp, Mp2
 
 #data = utils.loaddata('data/ep2epgamma')  
-
-# TODO
-m = Model.ComptonGepard()
-t = Approach.hotfixedBMK(m)
-
 # testing data set for fits
 #fitpoints = data[36]
 
+m = Model.ComptonGepard()
+t = Approach.hotfixedBMK(m)
+
+# Seting model parameters to be as in test.F
+def setpar(i, val):
+    t.m.parameters[t.m.parameters_index[i]] = val
+
+setpar(21, 0.4)
+setpar(11, 2./3. - 0.4)
+setpar(12, 1.1)
+setpar(13, 0.25)
+setpar(14, 1.1)
+setpar(22, 1.2)
+setpar(23, 0.25)
+setpar(24, 1.2)
+setpar(17, 0.0)
+setpar(27, 0.0)
+setpar(18, 0.0)
+setpar(28, 0.0)
+setpar(19, 0.0)
+setpar(29, 0.0)
+
+
 pt = Data.DummyPoint()
-pt.exptype = 'collider'
-pt.in1particle = 'e'
-pt.in1charge = 1
-pt.in1energy = 5.
-pt.in1polarizationvector = 'L'
-pt.in1polarization = 1.0
-pt.in2particle = 'p'
-pt.in2energy = 250.
-pt.s = 2 * pt.in1energy * (pt.in2energy + math.sqrt(
-    pt.in2energy**2 - Mp2)) + Mp2
-pt.W = 82.
-pt.Q2 = 1.
-pt.t = 0.0
-
-utils.fill_kinematics(pt)
-
-## Setting up parameters and initializing gepard
-g.readpar()
-g.parchr.fftype = np.array([c for c in 'SINGLET   '])
-g.init()
-
-g.par.par[21-1] = 0.4
-g.par.par[11-1] = 2./3. - 0.4
-g.par.par[12-1] = 1.1
-g.par.par[13-1] = 0.25
-g.par.par[14-1] = 1.1
-g.par.par[22-1] = 1.2
-g.par.par[23-1] = 0.25
-g.par.par[24-1] = 1.2
-
-#   turn off second PW
-g.par.par[17-1] = 0.0
-g.par.par[27-1] = 0.0
-
-#   turn off GPD E
-g.par.par[18-1] = 0.0
-g.par.par[28-1] = 0.0
-
-#   turn off xi-dependence
-g.par.par[19-1] = 0.0
-g.par.par[29-1] = 0.0
-
-
-def test_gepardF2():
-    """Calculate LO DIS F2 using gepard."""
-    g.parint.p = 0
-    g.parchr.process = np.array([c for c in 'DIS   '])
-    g.kinematics.del2 = 0.0
-
-    g.init()
-    g.getmbgpd()
-    g.hgrid.hgrid[0] = g.mbgpd.mbgpd
-
-    g.kinematics.xi = 0.002
-    g.kinematics.q2 = 1.0
-    g.nqs.nqsdis = 1
-    g.qs.qsdis[0] = 1.0
-    g.evolc(1,1)
-    g.f2f()
-    assert_almost_equal(g.f2.f2[g.parint.p], 0.65784282163635943)
-
-def test_gepardXDVCSevol():
-    """Calculate basic NLO DVCS cross section using gepard (+evolution)."""
-    g.parint.p = 1
-    g.parchr.process = np.array([c for c in 'DVCS  '])
-    g.init()
-
-    g.kinematics.w2 = 3.5**2
-    g.kinematics.q2 = 3.0
-    g.kinematics.xi = g.kinematics.q2 / ( 2.0 * g.kinematics.w2 + g.kinematics.q2)
-    g.mt.mtind = 0
-    g.nqs.nqs = 2
-    g.qs.qs[1] = g.kinematics.q2
-    g.evolc(1,2)
-    aux = g.sigma()
-    assert_almost_equal(aux, 6.8612469682766850, 5)
 
 
 def test_gepardXDVCSt():
+    """Calculate LO DVCS partial cross section using gepard (no evolution)."""
+    pt.W = 82.
+    pt.Q2 = 1.
+    pt.t = 0.0
+    pt.xi = pt.Q2 / ( 2.0 * pt.W * pt.W + pt.Q2)
+    t.m.g.parint.p = 0
+    t.m.g.init()
     aux = t.PartialCrossSection(pt)
     assert_almost_equal(aux, 5607.5998541187819, 2)
 
-#def test_gepardXDVCSt():
-#    g.parint.p = 0
-#    g.parchr.process = np.array([c for c in 'DVCS  '])
-#    g.init()
-#    
-#    g.kinematics.w2 = 82.0**2
-#    g.kinematics.q2 = 1.0
-#    g.kinematics.xi = g.kinematics.q2 / ( 2.0 * g.kinematics.w2 + g.kinematics.q2)
+def test_gepardXDVCSevol():
+    """Calculate basic NLO DVCS cross section using gepard (+evolution)."""
+    pt.W = 3.5
+    pt.Q2 = 3.
+    pt.t = 0.0
+    pt.xi = pt.Q2 / ( 2.0 * pt.W * pt.W + pt.Q2)
+    t.m.g.parint.p = 1
+    t.m.g.init()
+    aux = t.TotalCrossSection(pt)
+    assert_almost_equal(aux, 6.8606314494041793)
+    # To get complete agreement with Fortran take
+    # (slower) tquadrature = quadSciPy10 and:
+    # assert_almost_equal(aux, 6.8612469682766850, 5)
 
-#    g.nqs.nqs = 1
-#    g.qs.qs[0] = g.kinematics.q2
-#    g.evolc(1,1)
-
-#    g.mt.mtind = 0
-
-#    aux = g.parsigma()
-#    assert_almost_equal(aux, 5607.5998541187819, 2)
-
+#test_gepardXDVCSevol.newfeature = 1
 
 def test_gepardfitsimple():
     """Test simple fitting to H1 DVCS (one dataset) via gepard"""