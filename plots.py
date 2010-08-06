CHG: plotting COMPASS improved - might have sign bug

plots.py
@@ -372,13 +372,14 @@ def HALLAphi(lines=[], band=[], path=None, fmt='png'):
         fig.show()
     return fig
 
-def COMPASS(lines=[], band=[], path=None, fmt='png'):
+def COMPASS(lines=[], xB=0.05, Q2=2, path=None, fmt='png'):
     """Plot COMPASS BCS asymmetry and difference and summ of xs for FormFactors model ff.
     
     FIXME: kinematic completion, charge etc. must be explicit here
     """
 
-    title = 'COMPASS BCSA (asymmetry), BCSD (difference) and BCSS (sum) of XS'
+    title = 'COMPASS $Q^2$ = %s GeV$^2$, $x_B$ = %s' % (Q2, xB)
+    filename = 'COMPASS-Q2-%s-xB-%s' % (Q2, xB)
     fig = plt.figure()
     fig.canvas.set_window_title(title)
     fig.suptitle(title)
@@ -392,16 +393,15 @@ def COMPASS(lines=[], band=[], path=None, fmt='png'):
     pt.in1polarizationvector = 'L'
     pt.in1polarization = 0.8
     pt.s = 2 * Mp * pt.in1energy + Mp2
-    pt.xB = 0.05
+    pt.xB = xB
     pt.t = -0.2
-    pt.Q2 = 2.
+    pt.Q2 = Q2
     ax = fig.add_subplot(2,2,1)
     ax.axhline(y=0, linewidth=1, color='g')  # y=0 thin line
     phi = np.arange(0., np.pi, 0.2)
     utils.fill_kinematics(pt)
-    linestyles = ['g--', 'b-', 'r-.']
-    labels = ['', '', '']
-    #labels = ['GLO1 (DM)', 'GLO1 (KK)', '']
+    linestyles = ['g--', 'b-', 'c-.']
+    labels = ['without HALL-A', 'with HALL-A']
     pn = 0
     for approach in lines:
         approach.__class__.to_conventions(pt)
@@ -423,16 +423,13 @@ def COMPASS(lines=[], band=[], path=None, fmt='png'):
     pt.in1polarizationvector = 'L'
     pt.in1polarization = 0.8
     pt.s = 2 * Mp * pt.in1energy + Mp2
-    pt.xB = 0.05
+    pt.xB = xB
     pt.t = -0.2
-    pt.Q2 = 2.
+    pt.Q2 = Q2
     ax = fig.add_subplot(2,2,2)
     ax.axhline(y=0, linewidth=1, color='g')  # y=0 thin line
     phi = np.arange(0., np.pi, 0.2)
     utils.fill_kinematics(pt)
-    linestyles = ['g--', 'b-', 'r-.']
-    labels = ['', '', '']
-    #labels = ['GLO1 (DM)', 'GLO1 (KK)', '']
     pn = 0
     for approach in lines:
         approach.__class__.to_conventions(pt)
@@ -444,7 +441,7 @@ def COMPASS(lines=[], band=[], path=None, fmt='png'):
     #ax.set_ylim(0.0, 0.5)
     # axes labels
     ax.set_xlabel('$\\phi$')
-    ax.set_ylabel('BCS Difference  [pb]')
+    ax.set_ylabel('BCS Difference  [pb/GeV^4]')
     ax.legend()
     # Sum pannel
     pt = Data.DummyPoint()
@@ -455,17 +452,13 @@ def COMPASS(lines=[], band=[], path=None, fmt='png'):
     pt.in1polarizationvector = 'L'
     pt.in1polarization = 0.8
     pt.s = 2 * Mp * pt.in1energy + Mp2
-    pt.xB = 0.05
+    pt.xB = xB
     pt.t = -0.2
-    pt.Q2 = 2.
+    pt.Q2 = Q2
     utils.fill_kinematics(pt)
     ax = fig.add_subplot(2,2,3)
     ax.axhline(y=0, linewidth=1, color='g')  # y=0 thin line
     phi = np.arange(0., np.pi, 0.2)
-    linestyles = ['g--', 'b-', 'r-.']
-    #labels = ['HERMES+CLAS', 'HERMES+CLAS+HALLA', '+HALLA(phi)']
-    labels = ['', '', '']
-    #labels = ['GLO1 (DM)', 'GLO1 (KK)', '']
     pn = 0
     for approach in lines:
         approach.__class__.to_conventions(pt)
@@ -477,10 +470,12 @@ def COMPASS(lines=[], band=[], path=None, fmt='png'):
     #ax.set_ylim(0.0, 0.5)
     # axes labels
     ax.set_xlabel('$\\phi$')
-    ax.set_ylabel('BCS sum  [pb]')
+    ax.set_ylabel('BCS sum  [pb/GeV^4]')
     ax.legend()
     if path:
-        fig.savefig(os.path.join(path, title+'.'+fmt), format=fmt)
+        fig.subplots_adjust(hspace=0.15)
+        fig.subplots_adjust(wspace=0.35)
+        fig.savefig(os.path.join(path, filename+'.'+fmt), format=fmt)
     else:
         fig.canvas.draw()
         fig.show()
@@ -499,7 +494,7 @@ def COMPASSt(fits=[], path=None, fmt='png'):
     for xB, Q2 in kinpoints:
         ax = fig.add_subplot(2,3,panel)
         ax.axhline(y=0, linewidth=1, color='g')  # y=0 thin line
-        linestyles = ['g--', 'b-', 'r-.']
+        linestyles = ['r-', 'g--', 'b-.', 'p:']
         labels = ['HERMES+CLAS', 'HERMES+CLAS+HALLA', '+HALLA(phi)']
         pn = 0
         for approach in fits:
@@ -576,7 +571,7 @@ def EIC(fits=[], path=None, fmt='png'):
     ax.axhline(y=0, linewidth=1, color='g')  # y=0 thin line
     phi = np.arange(0., 2*np.pi, 0.2)
     utils.fill_kinematics(pt)
-    linestyles = ['b-.', 'p:', 'r-', 'g--'] 
+    linestyles = ['r-', 'g--', 'b-.', 'p:']
     labels = [r'$\sigma^{\uparrow}$ ' + t.name for t in fits]
     pn = 0
     for approach in fits:
@@ -585,7 +580,6 @@ def EIC(fits=[], path=None, fmt='png'):
         line = approach.Xunp(pt, vars={'phi':phi})
         ax.plot(phi, line, linestyles[pn], linewidth=1, label=labels[pn]) 
         pn += 1
-    linestyles = ['r-', 'g--', 'b-.', 'p:'] 
     labels = ['$(\\sigma^{\\uparrow} + \\sigma^{\\downarrow})/2$ ' + t.name for t in fits]
     pn = 0
     for approach in fits:
@@ -680,7 +674,7 @@ def HBCSA(ff, fits=[], path=None, fmt='png'):
     ax = fig.add_subplot(1,2,1)
     ax.set_xscale('log')  # x-axis to be logarithmic
     xval = np.power(10., np.arange(-3.5, 0, 0.01)) 
-    linestyles = ['g--', 'b-', 'r-.']
+    linestyles = ['r-', 'g--', 'b-.', 'p:'] 
     pn = 0
     for approach in fits:
         pt.t = 0.0
@@ -713,7 +707,6 @@ def HBCSA(ff, fits=[], path=None, fmt='png'):
     ax.axhline(y=0, linewidth=1, color='g')  # y=0 thin line
     phi = np.arange(0., np.pi, 0.2)
     utils.fill_kinematics(pt)
-    linestyles = ['g--', 'b-', 'r-.']
     pn = 0
     for approach in fits:
         approach.__class__.to_conventions(pt)

pype.py
@@ -8,6 +8,9 @@ import Model, Approach, Fitter
 import utils 
 import plots
 
+import Data
+from constants import Mp, Mp2
+
 from results import *
 
 # [1] Load experimental data and theoretical models
@@ -41,20 +44,69 @@ ALTGLO4points = data[25] + data[32][18:]
 
 ## [3] Create a theory
 
+# Gepard only
+#mGepard = Model.ComptonGepard(cutq2=0.5)
+#tGepard = Approach.hotfixedBMK(mGepard)
+
+
+# DR only
+mDRonly = Model.ModelDR()
+tDR = Approach.hotfixedBMK(mDRonly)
+tDR.name = 'DR model'
+
+mDRonly1 = Model.ModelDR()
+tDR1 = Approach.hotfixedBMK(mDRonly1)
+tDR1.name = 'DR model 1'
+
+
+# Hybrid: Gepard+DR (can reuse above Gepard)
+#mDRsea = Model.ComptonModelDRsea()
+#m = Model.Hybrid(mGepard, mDRsea)
+#t = Approach.hotfixedBMK(m)
+#t.name = 'DR + Gepard sea'
+#g = t.m.g
+
+
+tDR.m.parameters.update(DMepsGLO)
+tDR1.m.parameters.update(DMepsGLO1)
+
+#t.m.parameters.update(allTHI)
+
 # NN
 #mNN = Model.ModelNN(hidden_layers=[26], output_layer=['ImH', 'ReH', 'ImE', 'ReE', 'ImHt', 'ReHt', 'ImEt', 'ReEt'])
-mNN = Model.ModelNN(hidden_layers=[9], endpointpower=3.0)
-tNN = Approach.hotfixedBMK(mNN)
-tNN.name = 'NNtest'
-tNN.description = '(xB,t)-9-2 nets. Fit to ALTGLO4points'
+#mNN = Model.ModelNN(hidden_layers=[9], endpointpower=3.0)
+#tNN = Approach.hotfixedBMK(mNN)
+#tNN.name = 'NNtest'
+#tNN.description = '(xB,t)-9-2 nets. Fit to ALTGLO4points'
 
 ## [4] Do the fit
 
 
-f = Fitter.FitterBrain(ALTGLO4points, tNN, nnets=30, nbatch=20, verbose=1)
+#f = Fitter.FitterBrain(ALTGLO4points, tNN, nnets=30, nbatch=20, verbose=1)
 #f.fit()
 #f.prune(minprob=0.5)
 #tNN.save(db)
 #db.close()
 
+#t.m.release_parameters('M02S','SECG', 'THIS', 'THIG', 'rv', 'bv', 'Mv', 'C', 'MC', 'trv', 'tbv', 'tMv')
+#f = Fitter.FitterMinuit(DVCSpoints+data[48]+ALTGLO2points, t)
 
+pt = Data.DummyPoint()
+pt.exptype = 'fixed target'
+pt.in1particle = 'e'
+pt.in1charge = 1
+pt.in1energy = 160.
+pt.in1polarizationvector = 'L'
+pt.in1polarization = -0.4
+pt.s = 2 * Mp * pt.in1energy + Mp2
+pt.xB = 0.05
+pt.t = -0.2
+pt.Q2 = 2
+pt.phi = 0
+pt.frame = 'Trento'
+pt.units = {'phi' : 'radian'}
+utils.fill_kinematics(pt)
+tDR1.to_conventions(pt)
+tDR1.prepare(pt)
+#print tDR1.BCSA(pt, vars={'phi':np.pi})
+print tDR1.BCSA(pt)