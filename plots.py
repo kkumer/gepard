FIX: plotting error-bars from mean-std to mean+std ADD: covariance dictionary provided by pyminuit is now      used to calculate and plot uncertainty of CFFs

plots.py
@@ -99,8 +99,8 @@ def subplot(ax, sets, lines=[], band=[], xaxis=None, kinlabels=[], plotlines=Tru
                     nnet in range(len(band.model.nets))])
         mean = res.mean()
         std = res.std()
-        up.append(mean + std/2.)
-        down.append(mean - std/2.)
+        up.append(mean + std)
+        down.append(mean - std)
     up = np.array(up)
     down = np.array(down)
     # xval belongs to last set after loop above
@@ -1046,8 +1046,8 @@ def _axband(ax, tm, xvals, fun, color='g', avg=True):
     mean = res.mean(axis=0)
     std = res.std(axis=0)
     if avg:
-        up = mean + std/2.
-        down = mean - std/2.
+        up = mean + std
+        down = mean - std
     else:
         up = res.max(axis=0)
         down = res.min(axis=0)
@@ -1270,3 +1270,91 @@ def xBt(sets, path=None, fmt='png'):
         fig.canvas.draw()
         fig.show()
     return fig
+
+def uncertCFF(tnn=None, th=None, thband=None, cffs=['ImH'], path=None, fmt='png'):
+    """Makes plot of cffs given by theory th, with uncertainty band.
+    
+    tnn     -- neural net (NN) theory
+    th      -- non-NN theory to be plotted as line
+    thband  -- non-NN theory to be plotted as 1-sigma error band
+    cffs    -- List of CFFs to be plotted. Each produces two panels.
+
+    """
+    if tnn:
+        title = tnn.description
+        old = tnn.model.parameters['nnet']
+        tnn.model.parameters['nnet'] = 'ALL'
+    elif thband:
+        title = thband.description
+    elif th:
+        title = th.description
+    else:
+        title = 'uncertCFF'
+    fig = plt.figure()
+    fig.canvas.set_window_title(title)
+    fig.suptitle(title)
+    colors = ['red', 'brown']     # worm human colors :-)
+    nncolors = ['blue', 'green']  # cold computer colors
+    linestyles = ['solid', 'dashed']
+    tms = [0.2]
+    # Define abscissas
+    logxvals = np.power(10., np.arange(-3.0, -0.01, 0.1))  # left panel
+    xvals = np.linspace(0.025, 0.2, 20) # right panel
+    # ordinatas for  left ...
+    #allylims = [(-0.3, 1.0), (-0.3, 1.0), (-0.45, 0.05)]
+    #allylims = [(-3.3, 35.0), (-3.3, 10.0), (-4.45, 0.05)]
+    # ... and right panles
+    #ylims = [(-0.3, 1.0), (-0.3, 1.0), (-0.45, 0.05)]
+    #ylims = [(-3.3, 35.0), (-3.3, 10.0), (-4.45, 0.05)]
+    # Plot panels
+    for n in range(len(cffs)):
+        cff = cffs[n]
+        # all-x logarithmic
+        ax = fig.add_subplot(len(cffs), 2, 2*n+1)
+        ax.set_xscale('log')  # x-axis to be logarithmic
+        for tm in tms :
+            if tnn:
+                _axband(ax, tm, logxvals, getattr(tnn.model, cff), 
+                        color=nncolors[tms.index(tm)])
+            if th:
+                _axline(ax, tm, logxvals, getattr(th.model, cff), 
+                        color=colors[tms.index(tm)], 
+                        linestyle='dashed', label='t = %s' % str(tm))
+            if thband:
+                _axband(ax, tm, logxvals, lambda pt: thband.model.uncert(cff, pt), 
+                        color=colors[tms.index(tm)])
+        ax.set_xlabel('$\\xi$', fontsize=15)
+        ax.set_ylabel('%s' % cff, fontsize=18)
+        ax.axhspan(-0.0005, 0.0005, facecolor='g', alpha=0.6)  # horizontal bar
+        ax.axvspan(0.03, 0.093, facecolor='g', alpha=0.1)  # vertical band
+        ax.text(0.03, -0.27, "data region", fontsize=14)
+        #apply(ax.set_ylim, allylims[n])
+        ax.set_xlim(0.005, 1.0)
+        # measured x linear
+        ax = fig.add_subplot(len(cffs), 2, 2*n+2)
+        for tm in tms:
+            if tnn:
+                _axband(ax, tm, xvals, getattr(tnn.model, cff), 
+                        color=nncolors[tms.index(tm)])
+            if th:
+                _axline(ax, tm, xvals, getattr(th.model, cff), 
+                        color=colors[tms.index(tm)], 
+                        linestyle='dashed', label='t = %s' % str(tm))
+            if thband:
+                _axband(ax, tm, xvals, lambda pt: thband.model.uncert(cff, pt), 
+                        color=colors[tms.index(tm)])
+        ax.axhline(y=0, linewidth=1, color='g')  # y=0 thin line
+        ax.set_xlabel('$\\xi$', fontsize=15)
+        #apply(ax.set_ylim, ylims[n])
+        ax.set_xlim(0.03, 0.093)
+        ax.text(0.03, -0.27, "data region only", fontsize=14)
+        #ax.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(0.02))  # tickmarks
+    if tnn:
+        tnn.model.parameters['nnet'] = old
+    if path:
+        fig.savefig(os.path.join(path, title+'.'+fmt), format=fmt)
+    else:
+        fig.canvas.draw()
+        fig.show()
+    return fig
+

pype.py
@@ -3,7 +3,7 @@
 #import pylab
 #import matplotlib.pyplot as plt
 
-import shelve, copy
+import shelve, copy, sys
 
 import numpy as np
 import scipy.stats
@@ -121,13 +121,12 @@ mNN = Model.ModelNN(hidden_layers=[11])
 #mNN = Model.ModelNN(hidden_layers=[11], output_layer=['ImH', 'ReH', 'ReE','ImHt', 'ReHt', 'ReEt'])
 tNN = Approach.hotfixedBMK(mNN)
 tNN.name = 'NNtest'
-tNN.description = 'x (xB,t)-11-6 nets trained on HERMES+CLAS+HallA mock data for 100 batches'
+tNN.description = 'x (xB,t)-11-2 nets trained on HERMES-like mock data for 200 batches'
 
 ## [4] Do the fit
 #th.m.fix_parameters('ALL')
 
-#f = Fitter.FitterBrain(traindata, tNN, nnets=20, nbatch=200, verbose=1)
-#f = Fitter.FitterBrain(traindata, tNN, nnets=8, nbatch=400, verbose=2)
+#f = Fitter.FitterBrain(mockH, tNN, nnets=10, nbatch=200, verbose=1)
 #f.fit()
 #f.prune(minprob=0.5)
 #tNN.m.parameters['nnet'] = 'ALL'
@@ -148,8 +147,9 @@ tNN.description = 'x (xB,t)-11-6 nets trained on HERMES+CLAS+HallA mock data for
 
 
 ## DR fit
-th.m.release_parameters('rv', 'bv', 'Mv', 'C', 'MC')
-f = Fitter.FitterMinuit(mockH, th)
+tDR.m.release_parameters('rv', 'Mv')
+#tDR.m.release_parameters('rv', 'Mv')
+f = Fitter.FitterMinuit(mockH, tDR)
 
 ## [5] Some shortcuts ...
 
@@ -212,3 +212,48 @@ def ptcol(th, Q=-1, pol=1, Ee=5, Ep=250, xB=0.001, Q2=4., t=-0.2, phi=1., FTn=No
 
 ptc = ptcol(th, Q=1, pol=0, Ee=5, Ep=350, phi=3.14/2., xB=1e-2)
 
+def cov(f):
+    m = f.theory.model
+    # fitting parameters (not fixed)
+    pars = [p for p in m.parameter_names if m.parameters['fix_'+p] == False]
+    header = '     |' + len(pars)*'%5s   ' + '\n-----+' + len(pars)*'--------'+'\n'
+    sys.stdout.write(header % tuple(pars))
+    for prow in pars:
+        sys.stdout.write('%4s |' % prow)
+        for pcol in pars:
+            sys.stdout.write(' % 5.3f ' % f.minuit.covariance[prow, pcol])
+        sys.stdout.write('\n') 
+
+    
+def diff(cff, p, pt, h=0.05):
+    """Compute derivative of f w.r.t. model parameter p at point pt."""
+
+    m = cff.__self__  # m = instance of Model
+    mem = m.parameters[p]
+    m.parameters[p] = mem+h/2.
+    up = cff(pt)
+    m.parameters[p] = mem-h/2.
+    down = cff(pt)
+    m.parameters[p] = mem
+    return (up-down)/h
+
+def uncert(cff, covdict, pt):
+    """Uncertainty of cff propagated from covdict covariance dictionary."""
+
+    m = cff.__self__  # m = instance of Model
+    pars = [p for p in m.parameter_names if m.parameters['fix_'+p] == False]
+    var = 0
+    dfdp = {}
+    for p in pars:
+        dfdp[p] = diff(cff, p, pt, h=sqrt(covdict[p,p]))
+    for p1 in pars:
+        for p2 in pars:
+            var += dfdp[p1]*covdict[p1,p2]*dfdp[p2]
+    val = cff(pt)
+    std = sqrt(var)
+    return np.array([val-std, val+std])
+    
+    
+    
+
+    