03c7aa3c09d2a983b63f69f72b194aa95c24c7d2
Kresimir Kumericki <kkumer@phy.hr>
2012-02-01 17:15:31 +0000

    ADD: Gepard CFF and exp. ansaetze tests

---
+++ test/GepardCFF_test.py
@@ -0,0 +1,66 @@
+
+import copy
+from nose.tools import *
+import numpy as np
+
+import utils, Model, Approach, Data
+from results import hy1
+
+data = utils.loaddata('data/ep2epgamma', approach=Approach.hotfixedBMK)  
+
+m = Model.ComptonGepard()
+m.parameters.update(hy1)
+t = Approach.hotfixedBMK(m)
+
+# Seting model parameters to be as in test.F
+def setpar(i, val):
+    t.m.parameters[t.m.parameters_index[i]] = val
+
+# nloLOParameters
+
+setpar(11, 0.152039)
+setpar(12, 1.15751)
+setpar(13, 0.15)
+setpar(14, 0.478391)
+setpar(17, -0.15152)
+setpar(18, 0.7)
+setpar(19, 0.0)
+
+#setpar(21, 0.4)
+setpar(22, 1.24732)
+setpar(23, 0.15)
+setpar(24, 0.7)
+setpar(27, -0.81217)
+setpar(28, -0.2)
+setpar(29, 0.0)
+
+#setpar(111, 0.0)
+
+
+
+pt = Data.DummyPoint()
+
+
+def test_CFF():
+    """Calculate CFF H."""
+    pt.Q2 = 8.
+    pt.t = -0.2
+    pt.xi = 0.01
+    t.m.g.parint.p = 0
+    t.m.g.init()
+    assert_almost_equal(m.ImH(pt)/100, 66.5255/100, 4)
+    assert_almost_equal(m.ReH(pt)/100, 26.9984/100, 4)
+
+test_CFF.gepardsuite = 1
+
+def test_CFFE():
+    """Calculate CFF E."""
+    pt.Q2 = 8.
+    pt.t = -0.2
+    pt.xi = 0.01
+    t.m.g.parint.p = 0
+    t.m.g.init()
+    assert_almost_equal(m.ImE(pt)/100, 43.6024/100, 4)
+    assert_almost_equal(m.ReE(pt)/100, 13.5297/100, 4)
+
+test_CFF.gepardsuite = 1
