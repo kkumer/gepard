e051537abcd2e156d89100b39657d016dda68061
Kresimir Kumericki <kkumer@phy.hr>
2013-08-22 17:20:44 +0000

    CHG  From KO's to DM's formulas for NLO DVMP WIlson coefs. CHG  From MDILOG to MellinF2 (more accurate) in Harmonic.f DEL  MMA interface to DVEM

--- src/adacf/Harmonic.f
+++ src/adacf/Harmonic.f
@@ -157,15 +157,15 @@
 *
        IMPLICIT NONE
        INTEGER K
-       DOUBLE COMPLEX Z, MDILOG, HSM1
+       DOUBLE COMPLEX Z, MELLINF2, HSM1
        DOUBLE PRECISION EMC, ZETA2, ZETA3, LOG2
-       EXTERNAL HSM1, MDILOG
+       EXTERNAL HSM1, MELLINF2
        PARAMETER ( EMC  = 0.57721 56649D0, ZETA2 = 1.64493 40668D0,
      ,            ZETA3 = 1.20205 69032D0, LOG2 = 0.69314 718056D0 )
 *
 
         HSM21 = ZETA2*HSM1(Z,K) - 5.0d0*ZETA3/8.0d0 + ZETA2*LOG2
-     &     + (1-2*MOD(K+1,2)) * MDILOG(Z)
+     &     + (1-2*MOD(K+1,2)) * MELLINF2(Z+1)
               
 
        RETURN
@@ -288,3 +288,38 @@
        END
 *
 * =================================================================av==
+*
+C     Mellin transform  i.e. x^(N-1) moment of
+C        Li2(x)/(1+x)
+C     According to
+C        Eq. (33) in Bluemlein and Kurth, hep-ph/9708388
+C     This is equivalent to MDILOG(N-1), but more accurate (1e-7 or so)
+     
+      DOUBLE COMPLEX FUNCTION MELLINF2 (N)
+*
+      IMPLICIT NONE
+      INTEGER K
+      DOUBLE COMPLEX N, PSI, PSITMP, MF2
+      DOUBLE PRECISION LOG2, EMC, ZETA2, ABK(8)
+      PARAMETER ( LOG2 = 0.69314 71806D0, EMC  = 0.57721 56649D0,
+     &            ZETA2 = 1.64493 40668D0 )
+      DATA (ABK(K), K=1,8) / 0.9999964239d0, -0.4998741238d0, 
+     &   0.3317990258d0, -0.2407338084d0, 0.1676540711d0, 
+     &  -0.0953293897d0, 0.0360884937d0, -0.0064535442d0 /
+*
+*
+      PSITMP = PSI(N)
+      MF2 = (0.0d0, 0.0d0)
+*
+      DO 10 K = 1, 8
+         PSITMP = PSITMP + 1.0d0 / (N + K - 1)
+         MF2 = MF2 + ABK(K) * ( (N - 1) * ( ZETA2 / (N + K - 1) -
+     &         (PSITMP + EMC) / (N + K - 1)**2 ) +
+     &         (PSITMP + EMC) / (N + K - 1) )
+ 10   CONTINUE
+*
+      MELLINF2 =  ZETA2 * LOG2  -  MF2
+*
+      RETURN
+      END
+* =================================================================
