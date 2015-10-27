01a75c3802a5a2fe7330981885e3d592854101ab
Kresimir Kumericki <kkumer@phy.hr>
2015-10-27 22:00:30 +0100

    ADD fancy plots for Hall A data 2006 and 2015

--- pype/plots.py
+++ pype/plots.py
@@ -8,6 +8,7 @@ of some specific datasets and CFFs.
 
 import sys, os, math, copy, string, commands, itertools
 import numpy as np
+import pandas as pd
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -19,6 +20,8 @@ if os.sys.platform == 'win32':
 #import pylab as plt
 # Following works better in jupyter:
 matplotlib.rcParams['figure.figsize'] = (7.0, 5.0)
+
+from scipy.interpolate import InterpolatedUnivariateSpline
 
 
 import Data, Approach, utils
@@ -250,6 +253,23 @@ def panel(ax, points=None, lines=None, bands=None, xaxis=None, xs=None,
                 # If dataset doesn't have it, all points should have it 
                 labtxt += toTeX[lab] + ' = ' + str(getattr(points[0][0],lab)) + ', '
         ax.text(labx, laby, labtxt[:-2])
+
+def thline(th, pts, ex_pt, npts=16):
+    """Interpolated theory lines for kinematics in pandas dataframe pts and DataPoint ex_pt."""
+    tms = np.linspace(pts.tm.min(), pts.tm.max(), npts)
+    spl_xB = InterpolatedUnivariateSpline(pts.tm.values, pts.xB.values)
+    spl_Q2 = InterpolatedUnivariateSpline(pts.tm.values, pts.Q2.values)
+    ys = []
+    pt = copy.deepcopy(ex_pt)
+    for tm in tms:
+        pt.tm = tm
+        pt.xB = spl_xB(tm)[()]
+        pt.Q2 = spl_Q2(tm)[()]
+        del pt.t
+        del pt.W
+        utils.fill_kinematics(pt)
+        ys.append(th.predict(pt))
+    return tms, np.array(ys)
 
 #################################################################
 ##                                                             ##
@@ -951,6 +971,71 @@ def HallAFT(path=None, fmt='png', **kwargs):
         fig.show()
     return fig
 
+
+def HallA06(lines=None, path=None, fmt='png'):
+    NPTS = 12
+    # tupls are (dataset id, ind_low, ind_high)
+    xQbins = [1, 2, 3, 
+	      'blank', 'blank', 4,
+	      'blank', 'blank', 5]
+    subsets = {}
+    subsets[1] = utils.select(data[50], criteria=['Q2 == 1.5', 'FTn == -1'])
+    subsets[2] = utils.select(data[50], criteria=['Q2 == 1.9', 'FTn == -1'])
+    subsets[3] = utils.select(data[50], criteria=['Q2 == 2.3', 'FTn == -1'])
+    subsets[4] = utils.select(data[51], criteria=['FTn == 0'])
+    subsets[5] = utils.select(data[51], criteria=['FTn == 1'])
+    #fig, axs = plt.subplots(3, 3, sharey='row', sharex=True, figsize=[10,10])
+    fig, axs = plt.subplots(3, 3, figsize=[7,9])
+    axs = axs.reshape(9)
+    for pn, iset in enumerate(xQbins):
+	if iset == 'blank': 
+	    axs[pn].axis('off')
+	    continue
+	pts = pd.DataFrame([(pt.Q2, pt.xB, pt.tm, pt.val, pt.err) for pt in subsets[iset]], columns=('Q2', 'xB', 'tm', 'val', 'err'))
+	axs[pn].errorbar(pts.tm.values, pts.val.values, pts.err.values, linestyle='None')
+        if not isinstance(lines, list): lines = [lines]
+	styles = ['r-', 'k--', 'g:', 'b-.']
+	for nl, th in enumerate(lines):
+	    xs, ys = thline(th, pts, subsets[iset][0], npts=NPTS)
+	    axs[pn].plot(xs, ys, styles[nl], label=th.name)
+	axs[pn].set_xlim(0.12, 0.42)
+	axs[pn].xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(0.1)) 
+	s1 = r'$Q^2 =\, {:.2f}$'.format(pts.Q2.mean())
+	s1 += '\n'
+	s1 += r'$x_B =\, {:.2f}$'.format(pts.xB.mean())
+	if pn<3:
+	    axs[pn].text(0.25, 0.0175, s1, fontsize=14)
+	    axs[pn].set_ylim(0.0,0.024)
+	if pn in [4,5]:
+	    #axs[pn].text(0.25, 0.06, s1, fontsize=16)
+	    axs[pn].set_ylim(0.0,0.088)
+	    axs[pn].yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(0.03)) 
+	if pn in [7,8]:
+	    #axs[pn].text(0.25, 0.0005, s1, fontsize=16)
+	    axs[pn].set_ylim(-0.005, 0.017)
+	    axs[pn].axhline(y=0, linewidth=1, color='g')  # y=0 thin line
+	    axs[pn].yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(0.01))
+	#else:
+	    #axs[pn].text(0.22, 0.05, s1, fontsize=16)
+	if pn in [0,1,8]:
+	    axs[pn].set_xlabel(r'$-t\; [{\rm GeV}^2]$', fontsize=16)
+	if pn == 5:
+	    axs[pn].set_ylabel(r'$d\sigma^{\cos 0\phi,w}$', fontsize=20)
+	if pn == 8:
+	    axs[pn].set_ylabel(r'$d\sigma^{\cos \phi,w}$', fontsize=20)
+	    axs[pn].legend(loc=(-1.7,1.), fontsize=14, handlelength=3).draw_frame(0)
+	if pn == 0:
+	    axs[pn].set_ylabel(r'$\Delta\sigma^{\sin\phi,w}$', fontsize=20)
+	if pn in [1,2]:
+	    axs[pn].get_yaxis().set_visible(False)
+    fig.subplots_adjust(wspace=0.0, hspace=0.0) 
+    if path:
+        fig.savefig(os.path.join(path, title+'.'+fmt), format=fmt)
+    else:
+        fig.canvas.draw()
+        fig.show()
+    return fig
+
 def HallAphi(path=None, fmt='png', **kwargs):
     """Makes plot of Hall-A data phi-dependence with fit lines"""
 
@@ -1075,6 +1160,101 @@ def CLAS15phi(path=None, fmt='png', **kwargs):
         fig.canvas.draw()
         fig.show()
     return fig
+
+def CLAS15xs(lines=None, path=None, fmt='png'):
+    NPTS = 12
+    BSDw = pd.DataFrame([(pt.Q2, pt.xB, pt.tm, pt.val, pt.err) for pt in data[101]], columns=('Q2', 'xB', 'tm', 'val', 'err'))
+    xQbins = [(0,4), (5,9), (11,15), (16,20), (21,26), (30,33), (35,38), (39,42), (43,45), (46,47)]
+    fig, axs = plt.subplots(2,5, sharey='row', sharex=True, figsize=[12,9])
+    axs = axs.reshape(10)
+    for pn, (a,b) in enumerate(xQbins):
+	pts = BSDw[a:b+1]
+	axs[pn].errorbar(pts.tm.values, pts.val.values, pts.err.values, linestyle='None')
+        if not isinstance(lines, list): lines = [lines]
+	styles = ['r-', 'k--', 'g:', 'b-.']
+	for nl, th in enumerate(lines):
+	    xs, ys = thline(th, pts, data[101][a], npts=NPTS)
+	    axs[pn].plot(xs, ys, styles[nl], label=th.name)
+	axs[pn].set_xlim(0.05, 0.55)
+	axs[pn].xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(0.1)) 
+	s1 = '$Q^2 = {:.2f}$'.format(pts.Q2.mean())
+	s1 += '\n'
+	s1 += '$x_B = {:.2f}$'.format(pts.xB.mean())
+	if pn<5:
+	    axs[pn].text(0.2, 0.32, s1, fontsize=16)
+	else:
+	    axs[pn].text(0.22, 0.05, s1, fontsize=16)
+	if pn == 7:
+	    axs[pn].set_xlabel('$-t\\; [{\\rm GeV}^2]$', fontsize=16)
+	if (pn % 5) == 0:
+	    axs[pn].set_ylabel(r'$\Delta\sigma^{\sin\phi,w}$', fontsize=34)
+    fig.subplots_adjust(wspace=0.0, hspace=0.0) 
+    if path:
+        fig.savefig(os.path.join(path, title+'.'+fmt), format=fmt)
+    else:
+        fig.canvas.draw()
+        fig.show()
+    return fig
+
+
+def HallA15(lines=None, path=None, fmt='png'):
+    NPTS = 12
+    # tupls are (dataset id, ind_low, ind_high)
+    xQbins = [(117, 0, 4), (117, 5,9), (117, 10, 14), 
+	      ('blank', 0, 0), (116, 0, 4), (116, 5, 9),
+	      ('blank', 0, 0), (116, 10, 14), (116, 15, 19)]
+    #fig, axs = plt.subplots(3, 3, sharey='row', sharex=True, figsize=[8,10])
+    fig, axs = plt.subplots(3, 3, figsize=[7,9])
+    axs = axs.reshape(9)
+    for pn, (id, a,b) in enumerate(xQbins):
+	if id == 'blank': 
+	    axs[pn].axis('off')
+	    continue
+	pts = pd.DataFrame([(pt.Q2, pt.xB, pt.tm, pt.val, pt.err) for pt in data[id][a:b+1]], columns=('Q2', 'xB', 'tm', 'val', 'err'))
+	axs[pn].errorbar(pts.tm.values, pts.val.values, pts.err.values, linestyle='None')
+        if not isinstance(lines, list): lines = [lines]
+	styles = ['r-', 'k--', 'g:', 'b-.']
+	for nl, th in enumerate(lines):
+	    xs, ys = thline(th, pts, data[id][a], npts=NPTS)
+	    axs[pn].plot(xs, ys, styles[nl], label=th.name)
+	axs[pn].set_xlim(0.12, 0.42)
+	axs[pn].xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(0.1)) 
+	s1 = r'$Q^2 =\, {:.2f}$'.format(pts.Q2.mean())
+	s1 += '\n'
+	s1 += r'$x_B =\, {:.2f}$'.format(pts.xB.mean())
+	if pn<3:
+	    axs[pn].text(0.25, 0.015, s1, fontsize=14)
+	    axs[pn].set_ylim(0.0,0.02)
+	if pn in [4,5]:
+	    #axs[pn].text(0.25, 0.06, s1, fontsize=16)
+	    axs[pn].set_ylim(0.0,0.088)
+	    axs[pn].yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(0.03)) 
+	if pn in [7,8]:
+	    #axs[pn].text(0.25, 0.0005, s1, fontsize=16)
+	    axs[pn].set_ylim(-0.005, 0.017)
+	    axs[pn].axhline(y=0, linewidth=1, color='g')  # y=0 thin line
+	    axs[pn].yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(0.01))
+	#else:
+	    #axs[pn].text(0.22, 0.05, s1, fontsize=16)
+	if pn in [0,7,8]:
+	    axs[pn].set_xlabel(r'$-t\; [{\rm GeV}^2]$', fontsize=14)
+	if pn == 4:
+	    axs[pn].set_ylabel(r'$d\sigma^{\cos 0\phi,w}$', fontsize=20)
+	if pn == 7:
+	    axs[pn].set_ylabel(r'$d\sigma^{\cos \phi,w}$', fontsize=20)
+	    axs[pn].legend(loc=(-1.4,1.), fontsize=14, handlelength=3).draw_frame(0)
+	if pn == 0:
+	    axs[pn].set_ylabel(r'$\Delta\sigma^{\sin\phi,w}$', fontsize=20)
+	if pn in [1,2,5,8]:
+	    axs[pn].get_yaxis().set_visible(False)
+    fig.subplots_adjust(wspace=0.0, hspace=0.0) 
+    if path:
+        fig.savefig(os.path.join(path, title+'.'+fmt), format=fmt)
+    else:
+        fig.canvas.draw()
+        fig.show()
+    return fig
+
 
 def H1ZEUS(path=None, fmt='png', **kwargs):
     """Makes plot of H1 DVCS data with fit lines"""
