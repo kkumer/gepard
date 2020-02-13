90884226ee5e848334021f8281e31c738a6734b0
Kresimir Kumericki <kkumer@gmail.com>
2020-02-13 16:31:52 +0100

    ADD test NNet+dispersion relations

--- pype/test/NN_test.py
+++ pype/test/NN_test.py
@@ -6,16 +6,21 @@ import utils, Model, Approach, Fitter
 
 data = utils.loaddata('data/ep2epgamma', approach=Approach.hotfixedBMK)  
 db = shelve.open('theories.db', 'r')
+dbndr = shelve.open('nndr.db', 'r')
 
 
 # testing data set
 testpoints = [data[31][12]] + [data[8][1]] + [data[29][2]] + [data[30][3]]
 # testing data set for fits
 fitpoints = data[31] + data[8]
+fitpointsC = data[101][::12] + data[102][::12]
 
 # test-network 
 testNN = db['KMS11-NN']
 testNN.m.useDR = None
+
+# test-network using dispersion relations
+testNNDR = dbndr['NNDR-C15-8']
 
 
 def test_basicNN():
@@ -28,6 +33,16 @@ def test_basicNN():
 
 test_basicNN.long=1
 
+
+def test_NNDR():
+    """Testing Neural Net + dispersion relations."""
+    chisq = testNNDR.chisq(fitpointsC)[0]
+    assert_almost_equal(chisq, 10.800606014881728)
+
+test_NNDR.long=1
+test_NNDR.extendedtesting=1
+
+
 def test_fitNN():
     """Testing Neural Net fitting by FitterBrain."""
     numpy.random.seed(81)
@@ -38,6 +53,20 @@ def test_fitNN():
     chisq = tNN.chisq(fitpoints)[0]
     assert_almost_equal(chisq, 34.73003122832)
 
-# Actually this test was broken by Approach.observables consolidation
 test_fitNN.long = 1
+
+
+def test_fitNNDR():
+    """Testing Neural Net fitting by FitterBrain (DR+endpoint)."""
+    numpy.random.seed(81)
+    mNN = Model.ModelNN(output_layer=['ImH', 'ReH'],
+            useDR=['ReH'], endpointpower=1)
+    tNN = Approach.BMK(mNN)
+    fNN = Fitter.FitterBrain(fitpointsC, tNN, nnets=1, nbatch=12)
+    fNN.fit()
+    chisq = tNN.chisq(fitpointsC)[0]
+    assert_almost_equal(chisq, 12.131643143589963)
+
+# Actually this test was broken by Approach.observables consolidation
+test_fitNNDR.long = 1
 
