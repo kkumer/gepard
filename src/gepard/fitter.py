9f828196c81317ee9a79842fa137f0dfbae821bd
Kresimir Kumericki <kkumer@gmail.com>
2022-05-21 21:16:19 +0200

    improve fitter exception handling

--- src/gepard/fitter.py
+++ src/gepard/fitter.py
@@ -57,7 +57,7 @@ class MinuitFitter(Fitter):
             self.theory.covariance = {(p1, p2): self.minuit.covariance[p1, p2]
                                       for p1 in self.theory.free_parameters()
                                       for p2 in self.theory.free_parameters()}
-        except AttributeError:
+        except (AttributeError, TypeError) as error:
             print("Something's problematic. No covariances available.")
 
     def fit(self):
